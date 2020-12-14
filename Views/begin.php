c983f2babf042b921b805e1b8a6ed59f32cec0ad
petertoth406 <51701403+petertoth406@users.noreply.github.com>
2020-12-14 16:27:03 +0100

    Update begin.php

--- Views/begin.php
+++ Views/begin.php
@@ -5,7 +5,7 @@
   <title>Coronavirus Tracker</title>
   <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="style.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
